1bb9c6af3b7612cc2e865f12dbf716b71ced6493
mengxiang0811 <mengxiang0811@gmail.com>
2017-09-12 23:07:01 -0400

    cps: make all compile-time parameters runtime parameters

--- cps/kni.c
+++ cps/kni.c
@@ -34,12 +34,6 @@
 #include "gatekeeper_main.h"
 #include "kni.h"
 
-/* Number of times to attempt bring a KNI interface up or down. */
-#define NUM_ATTEMPTS_KNI_LINK_SET (5)
-
-/* Maximum number of updates for LPM table to serve at once. */
-#define MAX_CPS_ROUTE_UPDATES (8)
-
 /*
  * According to init_module(2) and delete_module(2), there
  * are no declarations for these functions in header files.
@@ -247,7 +241,7 @@ modify_link(struct mnl_socket *nl, struct rte_kni *kni,
 next:
 			attempts++;
 			sleep(1);
-		} while (attempts < NUM_ATTEMPTS_KNI_LINK_SET);
+		} while (attempts < get_cps_conf()->num_attempts_kni_link_set);
 	}
 kill:
 	/* Failed to wait for child or waited for too many attempts. */
@@ -616,7 +610,9 @@ mnl_socket_recvfrom_flags(const struct mnl_socket *nl, void *buf, size_t bufsiz,
 void
 kni_cps_route_event(struct cps_config *cps_conf)
 {
-	struct route_update updates[MAX_CPS_ROUTE_UPDATES];
+	uint16_t max_cps_route_updates =
+		get_cps_conf()->max_cps_route_updates;
+	struct route_update updates[max_cps_route_updates];
 	unsigned int num_updates = 0;
 	char buf[MNL_SOCKET_BUFFER_SIZE];
 
@@ -638,7 +634,7 @@ kni_cps_route_event(struct cps_config *cps_conf)
 
 		if (updates[num_updates].valid)
 			num_updates++;
-	} while (num_updates < MAX_CPS_ROUTE_UPDATES);
+	} while (num_updates < max_cps_route_updates);
 
 	/* TODO Send updates to GK block to update LPM table. */
 }
