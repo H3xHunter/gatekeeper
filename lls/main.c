mengxiang0811/gatekeeper-1

mailbox: make all compile-time parameters runtime parameters

lls/main.c
@@ -616,9 +616,9 @@ run_lls(struct net_config *net_conf, struct lls_config *lls_conf)
 		goto stage3;
 	}
 
-	ret = init_mailbox("lls_req", MAILBOX_MAX_ENTRIES,
-		sizeof(struct lls_request), lls_conf->lcore_id,
-		&lls_conf->requests);
+	ret = init_mailbox("lls_req", lls_conf->mailbox_max_entries,
+		sizeof(struct lls_request), lls_conf->mailbox_mem_cache_size,
+		lls_conf->lcore_id, &lls_conf->requests);
 	if (ret < 0)
 		goto timer;
 

sol/main.c
@@ -490,8 +490,8 @@ run_sol(struct net_config *net_conf, struct sol_config *sol_conf)
 	}
 
 	ret = init_mailbox("sol_reqs", 2 * sol_conf->pri_req_max_len,
-		sizeof(struct priority_req), sol_conf->lcore_id,
-		&sol_conf->mb);
+		sizeof(struct priority_req), sol_conf->mailbox_mem_cache_size,
+		sol_conf->lcore_id, &sol_conf->mb);
 	if (ret < 0)
 		goto out;
 

H3xHunter/gatekeeper

lls/main.c
@@ -616,9 +616,9 @@ run_lls(struct net_config *net_conf, struct lls_config *lls_conf)
 		goto stage3;
 	}
 
-	ret = init_mailbox("lls_req", MAILBOX_MAX_ENTRIES,
-		sizeof(struct lls_request), lls_conf->lcore_id,
-		&lls_conf->requests);
+	ret = init_mailbox("lls_req", net_conf->mailbox_max_entries,
+		sizeof(struct lls_request), net_conf->mailbox_mem_cache_size,
+		lls_conf->lcore_id, &lls_conf->requests);
 	if (ret < 0)
 		goto timer;
 

sol/main.c
@@ -490,8 +490,8 @@ run_sol(struct net_config *net_conf, struct sol_config *sol_conf)
 	}
 
 	ret = init_mailbox("sol_reqs", 2 * sol_conf->pri_req_max_len,
-		sizeof(struct priority_req), sol_conf->lcore_id,
-		&sol_conf->mb);
+		sizeof(struct priority_req), net_conf->mailbox_mem_cache_size,
+		sol_conf->lcore_id, &sol_conf->mb);
 	if (ret < 0)
 		goto out;
 